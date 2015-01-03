bf306e527dd7ee9f31d0fd68e2bf28f8a3fa88cd
sudheesh <sud4decrypt@gmail.com>
2015-01-03 17:02:06 +0530

    Message box for input and displaying mesaages

---
+++ mssgboxes.py
@@ -0,0 +1,33 @@
+from tkinter import *
+class SOMEMSG:
+    def __init__(self, parent,txt,temp):
+
+        top = self.top = Toplevel(parent)
+
+        Label(top, text=txt).pack()
+        self.temp=temp
+        self.e = Entry(top)
+        self.e.pack(padx=5)
+
+        b = Button(top, text="OK", command=self.ok)
+        b.pack(pady=5)
+
+    def ok(self):
+        self.temp.append(self.e.get())
+
+        self.top.destroy()
+
+
+class MSSG:
+    def __init__(self, parent,txt):
+
+        top = self.top = Toplevel(parent)
+
+        Label(top, text=txt).pack()
+
+        b = Button(top, text="OK", command=self.ok)
+        b.pack(pady=5)
+
+    def ok(self):
+        self.top.destroy()
+
